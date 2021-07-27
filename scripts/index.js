e165cf59d4912d843310d87fd536705bcd308f4c
Forejy <joffrey.marques@gmail.com>
2021-07-27 16:38:45 +0200

    Grid terminée avec l'algo le plus simple et le plus rapide + gestion du changement de couleur terminée

--- scripts/index.js
+++ scripts/index.js
@@ -1,106 +1,159 @@
-const squares = document.getElementsByClassName('square');
+// const squares = document.getElementsByClassName('square');
 
-console.log(squares);
-
-var color = 'red';
-
-function changeBackgroundColor (event, k) {
-  let tempColor = this.style.backgroundColor;
-  if((tempColor !== color))
-    this.style.backgroundColor = color;
-  else
-    this.style.backgroundColor = initialColor;
-}
-
-let initialColor0 = window.getComputedStyle(squares[0]).backgroundColor;
-
-for (var i = 0; i < squares.length; i++) {
-  let initialColor = window.getComputedStyle(squares[i]).backgroundColor;
-  squares[i].addEventListener("mouseover", function () {
-    let tempColor = this.style.backgroundColor;
-    if((tempColor !== color))
-      this.style.backgroundColor = color;
-    else
-      this.style.backgroundColor = initialColor;
-  });
-}
-
-
+// console.log(squares);
 const btnReset = document.getElementsByClassName('btn-reset')[0];
 const btnReset2 = document.getElementsByClassName('btn-reset2')[0];
-const modal = document.getElementsByClassName('modal')[0];
 const btnSizes = document.getElementsByClassName('btn-size');
+const initialColor = 'black';
 
+var a = 0;
+var colors = ['00', '00', '00'];
 
+// MARK: Change Colors
 
+function changeColors(rgb = ['00', '00', '00']) {
+  var temp;
+  var gap = 5;
+  var rgb0 = parseInt(rgb[0], 16);
+  var rgb1 = parseInt(rgb[1], 16);
+  var rgb2 = parseInt(rgb[2], 16);
 
-btnReset2.addEventListener('click', function () {
-  for (var i = 0; i < squares.length; i++) {
-    squares[i].classList.add('transition-bgc--fast');
-    squares[i].style.backgroundColor = initialColor0;
+  if ((rgb0 > 0) && (rgb0 == rgb1) && (rgb1 == rgb2)) {
+    console.log(rgb0);
+    temp = rgb0 - gap;
+    temp = temp <= 16 ? "0" + (rgb0 - gap).toString(16) : (rgb0 - gap).toString(16);
+    rgb[0] = temp;
+    rgb[1] = temp;
+    rgb[2] = temp;
+
   }
-  modal.style.display = 'block';
-})
+  else if (rgb0 < 255 && (rgb1 == 0 || rgb1 == 255)) {
+    temp = rgb0 + gap;
+    rgb[0] = temp <= 16 ? ("0" + temp.toString(16)) : (temp.toString(16));
+    console.log(rgb[0]);
+  }
+  else if (rgb1 < 255 && (rgb2 == 0 || rgb2 == 255)) {
+    temp = rgb1 + gap;
+    console.log(temp + 5);
+    rgb[1] = temp <= 16 ? ("0" + temp.toString(16)) : (temp.toString(16));
+    temp = rgb0 - gap;
+    console.log(temp);
+    rgb[0] = temp <= 16 ? ("0" + temp.toString(16)) : (temp.toString(16));
+    console.log(rgb[0]);
+  }
+  else if (rgb2 < 255) {
+    temp = rgb2 + gap;
+    rgb[2] = temp <= 16 ? ("0" + temp.toString(16)) : (temp.toString(16));
+    temp = rgb1 - gap;
+    rgb[1] = temp <= 16 ? ("0" + temp.toString(16)) : (temp.toString(16));
+  }
 
-btnReset2.addEventListener('click', function () {
-  setTimeout(function() {
-    for (var i = 0; i < squares.length; i++) {
-      squares[i].classList.remove('transition-bgc--fast');
+  return rgb;
+}
+
+
+function handleChangeBackgroundColor(squares = document.getElementsByClassName('square')){
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].addEventListener("mouseover", function () {
+      let tempColor = this.style.backgroundColor;
+      if((tempColor !== colors)) {
+        colors = (changeColors(colors));
+        str = "#" + colors[0] + colors[1] + colors[2];
+        console.log(str);
+        // console.log("#" + colors);
+        this.style.backgroundColor = "#" + colors[0] + colors[1] + colors[2];
+        // this.style.backgroundColor = "#" + colors;
+      }
+      else
+        this.style.backgroundColor = initialColor;
+    });
+  }
+}
+
+const modal = document.getElementsByClassName('modal')[0];
+
+function atStartOfPage() {
+  handleChangeBackgroundColor();
+  handleResetEvents();
+}
+
+atStartOfPage();
+
+// MARK: Handle Reset Events
+
+function handleResetEvents() {
+  let squares = document.getElementsByClassName('square');
+  let len = squares.length;
+
+  btnReset2.addEventListener('click', function handler1() {
+    for (k = 0; k <= len - 1; k++) {
+      squares[k].classList.add('transition-bgc--fast');
+      squares[k].style.backgroundColor = initialColor;
     }
-  }, 300)
-})
+    modal.style.display = 'block';
+    // modal.style.width = 100%;
+    // modal.style.height = 100%;
+    this.removeEventListener('click', handler1);
+  })
+
+  btnReset2.addEventListener('click', function handler2() {
+    setTimeout(function() {
+
+      for(l = 0; l <= len - 1; l++) {
+          squares[l].classList.remove('transition-bgc--fast');
+          l++;
+      }
+    }, 300)
+    this.removeEventListener('click', handler2);
+  })
+}
+
+
 
 const sizes = [10, 50, 96];
 
-function fun() {
+function handleGridSize() {
 
   for(var i = 0; i <= 2; i++) {
     let temp = sizes[i];
     btnSizes[i].addEventListener('click', function () {
-      // modal.style.display = 'none';
+      modal.style.display = 'none';
       var newGridCapacity = temp;
       var actualGrid = document.getElementsByClassName('grid')[0];
-      var newSquareSize = parseInt(window.getComputedStyle(actualGrid).width) / newGridCapacity;
+      var newSquareSize = ((parseInt(window.getComputedStyle(actualGrid).width) / newGridCapacity) - 1) + 'px';
       console.log(newSquareSize);
-      resizeGrid();
-      function resizeGrid(size) {
-        let actualGridRows = document.getElementsByClassName('grid-row');
-        let actualGridRowsSize = actualGridRows.length;
-        let row = actualGridRows[0].cloneNode([true]);
-        let extension = newGridCapacity - actualGridRowsSize;
-        // enlargeGrid();
-        resizeSquare();
 
 
-        function resizeSquare() {
-          for( i = 0; i <= actualGridRows.length - 1; i++) {
-            let tempGrid = actualGridRows[i];
-            let squares = tempGrid.getElementsByClassName('square');
-            tempGrid.style.height = '1px';
-            tempGrid.style.marginBottom = '1px'   ;
-            for( j = 0; j <= actualGridRows.length - 1; j++) {
-              squares[j].style.width = '1px'   ;
-              squares[j].style.height = '1px'   ;
-            }
-          }
+      changeGridSize();
+
+      function changeGridSize() {
+        gridRow = actualGrid.firstChild.cloneNode();
+
+        newGrid = actualGrid.cloneNode();
+        actualGrid.remove();
+
+        square = actualGrid.getElementsByClassName('square')[0].cloneNode();
+        square.style.width = newSquareSize;
+        square.style.height = newSquareSize;
+        gridRow.style.height = newSquareSize;
+
+        for(i = 1; i <= newGridCapacity; i++) {
+          gridRow.append(square);
+          square = square.cloneNode();
         }
-
-        function enlargeGrid() {
-          let square = actualGridRows[0].getElementsByClassName('square')[0].cloneNode();
-          console.log(square);
-          for( i = 0; i <= actualGridRowsSize - 1; i++) {
-            for( j = 0; j <= extension - 1; j++) {
-              actualGridRows[i].appendChild(square);
-              square = square.cloneNode();
-            }
-          }
+        for(j = 1; j <= newGridCapacity; j++) {
+          newGrid.append(gridRow);
+          gridRow = gridRow.cloneNode(true);
         }
+        document.getElementsByClassName('first-article')[0].append(newGrid);
       }
+      handleChangeBackgroundColor();
+      handleResetEvents();
     })
   }
 }
 
-fun();
+handleGridSize();
 // document.getElementsByClassName('grid')[0].appendChild(row);
 
 
